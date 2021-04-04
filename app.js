e14b2548fb5e67aa3a3e8aa3b2894ee5f4b559b7
pandeyanuradha <anuradha200219@gmail.com>
2021-04-05 01:48:25 +0530

    added post requests for dept and organ

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var urlencodedParser = bodyParser.urlencoded({extended: false})
 const db = mysql.createConnection({
   host     : 'localhost',//your ip address
   user     : 'root',//write your mysql user here
-  password : '****', //write your mysql password here
+  password : 'anuradha44@', //write your mysql password here
   database : 'dbms'//your database name
 }); 
 db.connect((err)=>{
@@ -199,6 +199,28 @@ app.get('/organ',(req,res)=>{
   })
 })
 
+app.post('/organ',urlencodedParser,(req,res)=>{
+  if(!req.body) return res.sendStatus(400)
+  const organid = req.body.organid
+  const quantity = req.body.quantity
+  const organ_name = req.body.organ_name
+
+  let sql="INSERT INTO organ values(?,?,?)"
+  db.query(sql,[organid,quantity,organ_name],(err,rows,fields)=>{
+    if(err)
+    {
+      console.log("Failed to post for the organ: "+err)
+      res.sendStatus(500)
+      res.end()
+      return
+    } 
+    console.log("I think we posted organ successfully")
+  })
+  //console.log(req.body.organid)
+  res.status(200).send("Created organ")
+})
+
+
 //DEPARTMENT TABLE
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.get('/department/:id',(req,res)=>{
@@ -232,6 +254,34 @@ app.get('/department',(req,res)=>{
     res.json(rows)
   })
 })
+
+app.post('/department',urlencodedParser,(req,res)=>{
+  if(!req.body) return res.sendStatus(400)
+  const dept_id =req.body.dept_id
+  const dept_name =req.body.dept_name
+  const capacity =req.body.capacity
+ 
+  
+  let sql="INSERT INTO department values(?,?,?)"
+  db.query(sql,[dept_id,dept_name,capacity],(err,rows,fields)=>{
+    if(err)
+    {
+      console.log("Failed to post for the department: "+err)
+      res.sendStatus(500)
+      res.end()
+      return
+    } 
+    console.log("I think we posted department successfully")
+  })
+  console.log(req.body.dob)
+  res.status(200).send("Created department")
+})
+
+// 
+
+// app.listen(3000,()=>{
+
+// })
 
 
 
